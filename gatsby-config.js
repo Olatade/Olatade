defe2c163ade04a403ae240120252f0e8833e25b
Olatade <4tade01@gmail.com>
2021-05-11 02:35:44 +0100

    Add google tag manager in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,15 +5,43 @@ dotenv.config({ path: '.env' });
 export default {
   siteMetadata: {
     title: `Olatade`,
-    siteUrl: 'https://olatade.com',
+    siteUrl: 'https://olatade.me',
     description: 'olatade\'s website',
-    twitter: '@ourpastiche',
+    twitter: '@4tade',
   },
   plugins: [
     'gatsby-plugin-react-helmet', // making sure our sever render has access to those
     'gatsby-plugin-styled-components',
     `gatsby-transformer-sharp`, // processes images
     `gatsby-plugin-sharp`, // processes images
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "	GTM-MNRC9KB",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+  
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
